15473d898ba09678a667b6dbb8483cecd4123c04
Anders Indrelid <anderhin@ntnu.no>
2021-03-19 15:12:04 +0100

    added main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+#This program's intented use is for a bachelor's thesis for Currence Robotics
+#It's used to identify different types of boxes or measure the gap between stacks of boxes.
+#Code by Anders Indrelid
+#
+import cv2 #imports opencv module for python
+import numpy #imports munpy module for python
+
+print('hello')
