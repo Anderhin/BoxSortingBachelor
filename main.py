137fc4ef0eacb076f05f585124ba490261dc9d44
Anders Indrelid <anderhin@ntnu.no>
2021-03-24 20:12:56 +0100

    Added cameraTest.py class
    Class is used for testing purposes of the Intel Realsense D415 camera

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ import numpy #imports munpy module for python
 
 imgPath = r'C:\Users\Ander\OneDrive\Dokumenter\BoxSortingPictures\BadExample.jpg'
 boxesExample = cv2.imread(imgPath,1)
-boxesExampleResize = cv2.resize(boxesExample, (1280,720))
-cv2.imshow('image',boxesExampleResize)
+#boxesExampleResize = cv2.resize(boxesExample, (1280,720))
+cv2.imshow('image',boxesExample)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-print('Beep Boop')
