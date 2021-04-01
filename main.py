4d6cbd61eeedd55f4e0a81637f030b926bfd7608
Anders Indrelid <anderhin@ntnu.no>
2021-04-01 13:29:11 +0200

    Added image processing for edge detection

--- main.py
+++ main.py
@@ -5,14 +5,24 @@
 import cv2 #imports opencv module for python
 import numpy as np #imports munpy module for python
 
-
-imgPath = r'C:\Users\Ander\OneDrive\Dokumenter\Python\BoxDetection\BoxSortingBachelor\BoxSortingPictures\BlueBoxFront_Color.png'
+#Set image path and read the image
+imgPath = r'C:\Users\Ander\OneDrive\Dokumenter\Python\BoxDetection\BoxSortingBachelor\BoxSortingPictures\BoxesFront_Color.png'
 image = cv2.imread(imgPath)
 
-window_name = 'image'
-image = image[200:650, 250:900]
+#Extract ROI for box
+image = image[120:700, 350:900]
 
-cv2.imshow(window_name, image)
+#Apply masks and filters for imageprocessing
+imgGray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),4)
+imgEdge = cv2.Canny(imgBlur,70,70)
 
+#Creates and shows image in a window
+cv2.imshow("Original", image)
+cv2.imshow("Grayscale",imgGray)
+cv2.imshow("Blurred",imgBlur)
+cv2.imshow("Edge",imgEdge)
+
+#Keeps image open until program is closed or "q" is pressed
 cv2.waitKey(0)
 cv2.destroyAllWindows(q)
